39195202c410013aba3e92f18b479f62bdc2b695
radowanTri <radowan.bhuiyan.tri@gmail.com>
2022-09-24 12:22:15 +0600

    details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,4 @@
 $(document).ready(function($) {
-    // "use strict";
-    // $("[data-background").each(function() {
-    //     $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
-    // });
-
-    // // Toggle the side navigation
-    // $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
-    //     $("body").toggleClass("sidebar-toggled");
-    //     $(".sidebar").toggleClass("toggled");
-    //     if ($(".sidebar").hasClass("toggled")) {
-    //         $('.sidebar .collapse').collapse('hide');
-    //     };
-    // });
-
-    // // Close any open menu accordions when window is resized below 768px
-    // $(window).resize(function() {
-    //     if ($(window).width() < 768) {
-    //         $('.sidebar .collapse').collapse('show');
-    //     };
-
-    //     // Toggle the side navigation when window is resized below 480px
-    //     if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
-    //         $("body").addClass("sidebar-toggled");
-    //         $(".sidebar").addClass("toggled");
-    //         $('.sidebar .collapse').collapse('show');
-    //     };
-    // });
 
     "use strict";
     $("[data-background").each(function() {
@@ -62,7 +35,6 @@ $(document).ready(function($) {
     var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })
-
 
 
     // Modal
